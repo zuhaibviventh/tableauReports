5d5044360b3f9f34f7000b8da171e87c58b770b6
manek1 <kiranmaneg207305@gmail.com>
2026-01-05 17:25:20 +0530

    Fixed hm immunization issue

--- routine_daily/clinical_operations/co_cp_hm_imm_alerts/sql/hm_immunization_alerts_nm.sql
+++ routine_daily/clinical_operations/co_cp_hm_imm_alerts/sql/hm_immunization_alerts_nm.sql
@@ -2,42 +2,32 @@ SET NOCOUNT ON;
 SET ANSI_WARNINGS OFF;
 
 
-IF OBJECT_ID('tempdb..#historical_hmt_immunizations__info') IS NOT NULL
-    DROP TABLE #historical_hmt_immunizations__info;
-SELECT HM_HISTORICAL_STATUS.PAT_ID,
+IF OBJECT_ID('tempdb..#hmt_immunizations__info') IS NOT NULL DROP TABLE #hmt_immunizations__info;
+SELECT PATIENT_HMT_STATUS.PAT_ID,
+       CAST(PATIENT_HMT_STATUS.IDEAL_RETURN_DT AS DATE) AS HM_IMM_DUE_DATE,
        SUBSTRING(CLARITY_HM_TOPIC.NAME, 5, LEN(CLARITY_HM_TOPIC.NAME)) AS HM_IMMUNIZATION_NAME,
-       CAST(HM_HISTORICAL_STATUS.LAST_COMPLETED_DATE AS DATE) AS HM_IMMUNIZATION_LAST_COMPLETED_DATE,
-       CAST(HM_HISTORICAL_STATUS.NEXT_DUE_DATE AS DATE) AS HM_IMMUNIZATION_NEXT_DUE_DATE,
        ZC_HMT_DUE_STATUS.NAME AS HM_IMMUNIZATION_STATUS
-INTO #historical_hmt_immunizations__info
-FROM Clarity.dbo.HM_HISTORICAL_STATUS_VIEW AS HM_HISTORICAL_STATUS
-    INNER JOIN Clarity.dbo.ZC_HMT_DUE_STATUS AS ZC_HMT_DUE_STATUS ON HM_HISTORICAL_STATUS.HM_STATUS_C = ZC_HMT_DUE_STATUS.HMT_DUE_STATUS_C
-    INNER JOIN CLARITY.dbo.CLARITY_HM_TOPIC AS CLARITY_HM_TOPIC ON HM_HISTORICAL_STATUS.HM_TOPIC_ID = CLARITY_HM_TOPIC.HM_TOPIC_ID
+INTO #hmt_immunizations__info
+FROM CLARITY.dbo.PATIENT_HMT_STATUS_VIEW AS PATIENT_HMT_STATUS
+    INNER JOIN CLARITY.dbo.ZC_HMT_DUE_STATUS AS ZC_HMT_DUE_STATUS ON PATIENT_HMT_STATUS.HMT_DUE_STATUS_C = ZC_HMT_DUE_STATUS.HMT_DUE_STATUS_C
+    INNER JOIN CLARITY.dbo.CLARITY_HM_TOPIC AS CLARITY_HM_TOPIC ON PATIENT_HMT_STATUS.QUALIFIED_HMT_ID = CLARITY_HM_TOPIC.HM_TOPIC_ID
 WHERE CLARITY_HM_TOPIC.HM_TOPIC_ID IN ( 209, 120, 107, 108, 105, 100, 141, 12, 109, 130, 112, 132, 110, 151, 155, 153, 113, 160, 161, 162, 133, 104, 54, 134,
                                         103, 131, 46, 135 )
-      AND ZC_HMT_DUE_STATUS.HMT_DUE_STATUS_C = 4
-GROUP BY HM_HISTORICAL_STATUS.PAT_ID,
-         CLARITY_HM_TOPIC.NAME,
-         HM_HISTORICAL_STATUS.LAST_COMPLETED_DATE,
-         HM_HISTORICAL_STATUS.NEXT_DUE_DATE,
-         ZC_HMT_DUE_STATUS.NAME
-ORDER BY HM_HISTORICAL_STATUS.PAT_ID,
-         CLARITY_HM_TOPIC.NAME,
-         HM_HISTORICAL_STATUS.NEXT_DUE_DATE DESC;
+      /* Incomplete */
+      --AND ZC_HMT_DUE_STATUS.HMT_DUE_STATUS_C != 6;
 
 
 IF OBJECT_ID('tempdb..#hmt_immunizations') IS NOT NULL DROP TABLE #hmt_immunizations;
-SELECT #historical_hmt_immunizations__info.PAT_ID,
-       #historical_hmt_immunizations__info.HM_IMMUNIZATION_LAST_COMPLETED_DATE,
-       #historical_hmt_immunizations__info.HM_IMMUNIZATION_NEXT_DUE_DATE,
-       CASE WHEN #historical_hmt_immunizations__info.HM_IMMUNIZATION_NAME = 'Meningococcal' THEN 'Meningo.'
-           WHEN #historical_hmt_immunizations__info.HM_IMMUNIZATION_NAME = 'Meningococcal B' THEN 'Meningo. B'
-           WHEN #historical_hmt_immunizations__info.HM_IMMUNIZATION_NAME = 'Mpox (formerly Monkeypox)' THEN 'Mpox'
-           ELSE #historical_hmt_immunizations__info.HM_IMMUNIZATION_NAME
+SELECT #hmt_immunizations__info.PAT_ID,
+       #hmt_immunizations__info.HM_IMM_DUE_DATE,
+       CASE WHEN #hmt_immunizations__info.HM_IMMUNIZATION_NAME = 'Meningococcal' THEN 'Meningo.'
+           WHEN #hmt_immunizations__info.HM_IMMUNIZATION_NAME = 'Meningococcal B' THEN 'Meningo. B'
+           WHEN #hmt_immunizations__info.HM_IMMUNIZATION_NAME = 'Mpox (formerly Monkeypox)' THEN 'Mpox'
+           ELSE #hmt_immunizations__info.HM_IMMUNIZATION_NAME
        END AS HM_IMMUNIZATION_NAME,
-       #historical_hmt_immunizations__info.HM_IMMUNIZATION_STATUS
+       #hmt_immunizations__info.HM_IMMUNIZATION_STATUS
 INTO #hmt_immunizations
-FROM #historical_hmt_immunizations__info;
+FROM #hmt_immunizations__info;
 
 
 IF OBJECT_ID('tempdb..#delivery') IS NOT NULL DROP TABLE #delivery;
@@ -46,6 +36,9 @@ SELECT IDENTITY_ID.IDENTITY_ID AS MRN,
        CAST(PATIENT.BIRTH_DATE AS DATE) AS DOB,
        ##most_recent_visit.CITY,
        ##most_recent_visit.STATE,
+       ##most_recent_visit.SERVICE_TYPE,
+       ##most_recent_visit.SERVICE_LINE,
+       ##most_recent_visit.SUB_SERVICE_LINE,
        ##most_recent_visit.VISIT_DATE AS LATEST_VISIT_DATE,
        ##most_recent_visit.VISIT_DEPARTMENT_NAME,
        COALESCE(zc_gender_identity.NAME, 'Unknown') AS GENDER,
@@ -56,8 +49,7 @@ SELECT IDENTITY_ID.IDENTITY_ID AS MRN,
            ELSE 'Other'
        END AS PATIENT_TYPE,
        COALESCE(CLARITY_SER.PROV_NAME, 'NO PCP ASSIGNED') AS PCP,
-       #hmt_immunizations.HM_IMMUNIZATION_LAST_COMPLETED_DATE,
-       #hmt_immunizations.HM_IMMUNIZATION_NEXT_DUE_DATE,
+       #hmt_immunizations.HM_IMM_DUE_DATE,
        #hmt_immunizations.HM_IMMUNIZATION_NAME,
        #hmt_immunizations.HM_IMMUNIZATION_STATUS,
        ##next_medical_appointment.NEXT_MEDICAL_APPOINTMENT,
@@ -72,9 +64,9 @@ FROM Clarity.dbo.PATIENT_VIEW AS PATIENT
     INNER JOIN Clarity.dbo.IDENTITY_ID_VIEW AS IDENTITY_ID ON PATIENT.PAT_ID = IDENTITY_ID.PAT_ID
     INNER JOIN #hmt_immunizations ON PATIENT.PAT_ID = #hmt_immunizations.PAT_ID
     INNER JOIN ##most_recent_visit ON PATIENT.PAT_ID = ##most_recent_visit.PAT_ID
-                                      AND ##most_recent_visit.ROW_NUM_DESC = 1
+                                     AND ##most_recent_visit.ROW_NUM_DESC = 1
     LEFT JOIN ##next_medical_appointment ON ##next_medical_appointment.PAT_ID = PATIENT.PAT_ID
-                                            AND ##next_medical_appointment.RN_ASC = 1
+                                          AND ##next_medical_appointment.RN_ASC = 1
     LEFT JOIN CLARITY.dbo.PATIENT_4 AS patient_4 ON PATIENT.PAT_ID = patient_4.PAT_ID
     LEFT JOIN CLARITY.dbo.ZC_GENDER_IDENTITY AS zc_gender_identity ON patient_4.GENDER_IDENTITY_C = zc_gender_identity.GENDER_IDENTITY_C
     LEFT JOIN CLARITY.dbo.PATIENT_RACE AS patient_race ON patient.PAT_ID = patient_race.PAT_ID
@@ -94,40 +86,19 @@ GROUP BY PATIENT.BIRTH_DATE,
          IDENTITY_ID.IDENTITY_ID,
          PATIENT.PAT_NAME,
          CLARITY_SER.PROV_NAME,
-         #hmt_immunizations.HM_IMMUNIZATION_LAST_COMPLETED_DATE,
-         #hmt_immunizations.HM_IMMUNIZATION_NEXT_DUE_DATE,
-         #hmt_immunizations.HM_IMMUNIZATION_NAME,
-         #hmt_immunizations.HM_IMMUNIZATION_STATUS,
+         HM_IMM_DUE_DATE,
+         HM_IMMUNIZATION_NAME,
+         HM_IMMUNIZATION_STATUS,
          ##next_medical_appointment.NEXT_MEDICAL_APPOINTMENT,
          ##next_medical_appointment.NEXT_APPT_PROVIDER,
          ##cp_cohorts.CP_COHORT,
          ##most_recent_visit.CITY,
          ##most_recent_visit.STATE,
+         ##most_recent_visit.SERVICE_TYPE,
+         ##most_recent_visit.SERVICE_LINE,
+         ##most_recent_visit.SUB_SERVICE_LINE,
          ##most_recent_visit.VISIT_DATE,
          ##most_recent_visit.VISIT_DEPARTMENT_NAME;
 
 
-SELECT #delivery.MRN,
-       #delivery.PAT_NAME,
-       #delivery.DOB,
-       #delivery.CITY,
-       #delivery.STATE,
-       #delivery.LATEST_VISIT_DATE,
-       #delivery.VISIT_DEPARTMENT_NAME,
-       #delivery.GENDER,
-       #delivery.RACE,
-       #delivery.ETHNICITY,
-       #delivery.PATIENT_TYPE,
-       #delivery.PCP,
-       #delivery.HM_IMMUNIZATION_LAST_COMPLETED_DATE,
-       #delivery.HM_IMMUNIZATION_NEXT_DUE_DATE,
-       #delivery.HM_IMMUNIZATION_NAME,
-       #delivery.HM_IMMUNIZATION_STATUS,
-       #delivery.NEXT_MEDICAL_APPOINTMENT,
-       #delivery.NEXT_APPT_PROVIDER,
-       #delivery.CP_COHORT,
-       #delivery.LAST_VISIT_WITHIN_13_MO,
-       #delivery.UPDATE_DTTM
-FROM #delivery
-WHERE DATEDIFF(MONTH, LATEST_VISIT_DATE, CURRENT_TIMESTAMP) <= 36
-      AND DATEDIFF(MONTH, HM_IMMUNIZATION_NEXT_DUE_DATE, CURRENT_TIMESTAMP) <= 36;
+SELECT * FROM #delivery WHERE DATEDIFF(MONTH, LATEST_VISIT_DATE, CURRENT_TIMESTAMP) <= 36 ORDER BY LATEST_VISIT_DATE ASC;
